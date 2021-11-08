34a5033b079635e33988fff4b5f67314400e1312
Vignesh <vignesh.viky94@gmail.com>
2021-11-09 02:27:34 +0530

    final commit

---
+++ src/test/java/qaclickacademy/SampleTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void APILogin()
+	{
+		System.out.println("SampleLogin");
+	}
+
+	@Test
+	public void DataSheet()
+	{
+		System.out.println("DataSheet");
+    }
+}
