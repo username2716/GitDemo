239eb8a7ff5f519d72b391f791faf508dcc7e9c7
Vignesh <vignesh.viky94@gmail.com>
2021-11-09 01:46:50 +0530

    small change

---
+++ src/main/java/qaclickacademy/RestAPI.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+public class RestAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+       System.out.println("Rest API test 1" );
+        
+       System.out.println("Rest API test 2" );
+        
+       System.out.println("Rest API test 3" );
+        
+       System.out.println("Rest API test 4" );
+	}
+
+}
