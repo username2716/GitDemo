46dfaaa3232cb705844638c24d41e6922c6ff473
Vignesh <vignesh.viky94@gmail.com>
2021-11-09 02:17:11 +0530

    working on branch

---
+++ src/main/java/qaclickacademy/Seleniumdemo.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+public class Seleniumdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+       System.out.println("Selenium demo test 1" );
+        
+       System.out.println("selenium demo test 2" );
+        
+       System.out.println("selenium demo test 3" );
+        
+       System.out.println("selenium demo test 4" );
+	}
+}
